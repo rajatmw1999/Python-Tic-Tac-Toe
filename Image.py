52c8a96eaab61a4a9c413443da3bc7b4dc1017ef
Rajat Upadhyay <43850934+rajatmw1999@users.noreply.github.com>
2020-09-30 22:59:58 +0530

    Images Code

---
+++ Image.py
@@ -0,0 +1,8 @@
+#loading the images
+opening = pg.image.load('tic tac opening.png')
+x_img = pg.image.load('x.png')
+o_img = pg.image.load('o.png')
+#resizing images
+x_img = pg.transform.scale(x_img, (80,80))
+o_img = pg.transform.scale(o_img, (80,80))
+opening = pg.transform.scale(opening, (width, height+100))
