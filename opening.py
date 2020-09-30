2a827dd0fa51945d4b3364fff31c57d3dae402c1
Rajat Upadhyay <43850934+rajatmw1999@users.noreply.github.com>
2020-09-30 23:01:01 +0530

    Game opening functions

---
+++ opening.py
@@ -0,0 +1,12 @@
+def game_opening():
+    screen.blit(opening,(0,0))
+    pg.display.update()
+    time.sleep(1)
+    screen.fill(white)
+    # Drawing vertical lines
+    pg.draw.line(screen,line_color,(width/3,0),(width/3, height),7)
+    pg.draw.line(screen,line_color,(width/3*2,0),(width/3*2, height),7)
+    # Drawing horizontal lines
+    pg.draw.line(screen,line_color,(0,height/3),(width, height/3),7)
+    pg.draw.line(screen,line_color,(0,height/3*2),(width, height/3*2),7)
+    draw_status()
